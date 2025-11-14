fe84a16e15de66ee0a71455da926ce3f4ba262fc
liqirong02 <liqirong02@outlook.com>
2025-11-14 07:45:19 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flash import Flash
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return (render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
